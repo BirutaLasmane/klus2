79eb8468d2541e89edad35ddc3fe63f3baa6507c
BirutaLasmane <biruta.lasmane@jvg.edu.lv>
2020-10-21 15:48:46 +0300

    pievienoju API

--- js/main.js
+++ js/main.js
@@ -18,3 +18,35 @@ aprikojumsPoga.addEventListener('click', function() {
         vielasRindas[i].classList.toggle('slepts');
     }
 });
+
+async function iegutVielasNoApi()
+{
+    let datiNoApi = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/vielas')
+    let datiJson = await datiNoApi.json();
+    return datiJson;
+}
+
+async function iegutInventaruNoApi()
+{
+    let datiNoApi = await fetch('https://pytonc.eu.pythonanywhere.com/api/v1/inventars')
+    let datiJson = await datiNoApi.json();
+    return datiJson;
+}
+
+async function raditDatus()
+{
+    let vielasJson = await iegutVielasNoApi();
+    let inventarsJson = await iegutInventaruNoApi();
+
+    let produktiKopa = vielasJson.concat(inventarsJson);
+
+    
+
+    for (let i = 0; i < produktiKopa.length; i++)
+    {
+        console.log( produktiKopa[i]['title'] );
+    }
+    
+}
+
+raditDatus();
